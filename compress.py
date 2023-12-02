79ecf543a52fc3a600eab4620f1168e19f04268d
1NF1N17Y <57046419+Dark1NF1N17Y@users.noreply.github.com>
2023-12-02 09:21:26 +0200

    Create compress.py

---
+++ compress.py
@@ -0,0 +1,28 @@
+import os, ffmpeg
+filename = input("Video Name With Extension ( .mp4 ) And Path If Needed , Add \ For Space In Path : ");
+outputname = input("Output Name With Extension ( .mp4 ) : ");
+def compress_video(video_full_path, output_file_name, target_size):
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+    probe = ffmpeg.probe(video_full_path)
+    duration = float(probe['format']['duration'])
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+
+    if 10 * audio_bitrate > target_total_bitrate:
+        audio_bitrate = target_total_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+    video_bitrate = target_total_bitrate - audio_bitrate
+
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, os.devnull,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                  ).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  ).overwrite_output().run()
+
+compress_video(filename, outputname, 50 * 1000)
