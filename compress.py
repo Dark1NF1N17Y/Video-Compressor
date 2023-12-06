91720ea48f467c5f4081b28b652d2cbc9a1017f8
1NF1N17Y <57046419+Dark1NF1N17Y@users.noreply.github.com>
2023-12-06 18:00:19 +0200

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,6 +1,5 @@
-import os, ffmpeg
-filename = input("Video Name With Extension ( .mp4 ) And Path If Needed , Add \ For Space In Path : ");
-outputname = input("Output Name With Extension ( .mp4 ) : ");
+import os, ffmpeg, time
+
 def compress_video(video_full_path, output_file_name, target_size):
     min_audio_bitrate = 32000
     max_audio_bitrate = 256000
@@ -22,7 +21,32 @@ def compress_video(video_full_path, output_file_name, target_size):
                   **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
                   ).overwrite_output().run()
     ffmpeg.output(i, output_file_name,
-                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'c:a': 'aac', 'b:a': audio_bitrate}
                   ).overwrite_output().run()
 
-compress_video(filename, outputname, 50 * 1000)
+def input_part():
+    os.system('cls||clear')
+    print("=" * 50)
+    input1 = input("Enter Video Name : ")
+    if input1.endswith('.mp4'):
+        filename = (input1)
+    else:
+        filename = (input1 + ".mp4")
+    if os.path.isfile(filename):
+        input2 = input("Enter Output Name : ")
+        print("=" * 50)
+        if input2.endswith('.mp4'):
+            outputname = (input2)
+        else:
+            outputname = (input2 + ".mp4")
+        compress_video(filename, outputname, 50 * 1000)
+    else:
+        os.system('cls||clear')
+        print("=" * 50)
+        print("Video Not Found! Try Again _/(^ ^)\_")
+        print("=" * 50)
+        time.sleep(0.7)
+        os.system('cls||clear')
+        input_part()
+
+input_part()
